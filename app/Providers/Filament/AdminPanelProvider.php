5654a3067b87bf083037a13c31a96481fd67908d
admin <locuxtech@gmail.com>
2026-06-21 17:23:10 -0500

    fix: register ChatBot page explicitly in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use App\Filament\Pages\ChatBot;
 use App\Filament\Pages\Comandas;
 use App\Filament\Auth\EditProfile;
 use App\Models\NegocioSetting;
@@ -53,6 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
                 Comandas::class,
+                ChatBot::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
